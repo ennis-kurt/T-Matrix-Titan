387865726e837dbbbae079be48fdb56b177d099e
cihatkurt <46458661+cihatkurt@users.noreply.github.com>
2022-04-07 22:08:14 -0400

    Jupyter notebook for model building created

--- special/data/model_training.py
+++ special/data/model_training.py
@@ -9,7 +9,7 @@ Created on Tue Apr  5 16:25:22 2022
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sn
+import seaborn as sns
 # %%
 
 import os
@@ -29,3 +29,7 @@ df.plot(x='p2', y='p3', kind='scatter')
 # Coefficients depend on each other as expected, not not strongly though.
 # Above plotting shows that.
 # If synthetic data will be created we should create p1,p2,p3 all together.
+# let's plot pairwise correlation heatmap
+f, ax = plt.subplots(figsize=(14, 10))
+ax = sns.heatmap(df.corr(), annot=True, cmap='PuOr')
+plt.show()
